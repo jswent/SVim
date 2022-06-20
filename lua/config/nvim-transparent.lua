a19d7760a1989bfe5878972d8861cdcedd25c042
James Swent <jake@osprey21.com>
2022-06-20 02:09:38 -0400

    disabled nvim-transparent on startup

--- lua/config/nvim-transparent.lua
+++ lua/config/nvim-transparent.lua
@@ -1,5 +1,5 @@
 require("transparent").setup({
-  enable = true, -- boolean: enable transparent
+  enable = false, -- boolean: enable transparent
   extra_groups = { -- table/string: additional groups that should be cleared
     -- In particular, when you set it to 'all', that means all available groups
 
@@ -11,5 +11,6 @@ require("transparent").setup({
     "BufferLineSeparator",
     "BufferLineIndicatorSelected",
   },
+  --extra_groups = "all",
   exclude = {}, -- table: groups you don't want to clear
 })
