568a49590392f94cbc96eda4bb56f24b92fe12ad
James Swent <jake@osprey21.com>
2022-06-24 14:29:43 -0400

    added additional file detection

---
+++ ftdetect/filetype.lua
@@ -0,0 +1,10 @@
+-- Additional filetypes that must be autodetected
+
+vim.cmd [[
+  
+  -- rofi configuration files 
+  au BufRead,BufNewFile *.rasi set filetype=rofi
+  
+  -- .aliases, .bash_aliases, etc.
+  au BufRead,BufNewFile .*aliases set filetype=sh
+]]
