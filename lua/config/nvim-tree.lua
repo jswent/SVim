60368739d5729c34c290338267580f2a9d9e1dd8
jswent <94636565+jswent@users.noreply.github.com>
2022-07-13 02:09:03 -0400

    arrows

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -32,7 +32,7 @@ nvim_tree.setup {
           ignored = "◌",
         },
         folder = {
-          arrow_open = " ",
+          arrow_open = "",
           arrow_closed = "",
           default = "",
           open = "",
