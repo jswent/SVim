2b48305a1d63d3f070696ea90f782e113d8902c3
jswent <94636565+jswent@users.noreply.github.com>
2022-09-23 09:31:06 +0100

    changed telescope displays

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,85 +1,208 @@
-local actions = require('telescope.actions')
-require('telescope').setup {
-  defaults = {
-    prompt_prefix = "   ",
-    selection_caret = "  ",
-    entry_prefix = "  ",
-    initial_mode = "insert",
-    selection_strategy = "reset",
-    sorting_strategy = "ascending",
-    layout_strategy = "horizontal",
-    layout_config = {
-        horizontal = {
-            prompt_position = "top",
-            preview_width = 0.55,
-            results_width = 0.8
-        },
-        vertical = {
-            mirror = false
-        },
-        width = 0.95,
-        height = 0.90,
-        preview_cutoff = 120
-    },
-    vimgrep_arguments = {
-        "rg",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-        "--hidden"
-    },
-    file_sorter = require("telescope.sorters").get_fuzzy_file,
-    file_ignore_patterns = {
-        "node_modules",
-        ".git",
-        "target",
-        "vendor"
-    },
-    generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-    path_display = {shorten = 5},
-    winblend = 0,
-    border = {},
-    borderchars = {"─", "│", "─", "│", "┌", "┐", "┘", "└"},
-    color_devicons = true,
-    set_env = {["COLORTERM"] = "truecolor"},
-    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-    buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker
-    },
-    pickers = {
-        file_browser = {
-            hidden = true
-        },
-        find_files = {
-            hidden = true
-        },
-        live_grep = {
-            hidden = true,
-            only_sort_text = true
-        }
-    },
-    extensions = {
-        fzf = {
-            fuzzy = true,
-            override_generic_sorter = false,
-            override_file_sorter = true,
-            case_mode = "smart_case"
-        },
-        file_browser = {
-            theme = "ivy"
-        },
-        bookmarks = {
-            selected_browser = "brave",
-            url_open_plugin = "open_browser"
-        }
-    }
-  }
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+  return
+end
 
-require('telescope').load_extension "ui-select"
-require('telescope').load_extension "file_browser"
-require('telescope').load_extension "notify"
---require('telescope').load_extension "project"
+local actions = require "telescope.actions"
+--telescope.load_extension "media_files"
+local icons = require "config.icons"
+
+telescope.setup {
+  defaults = {
+
+    prompt_prefix = icons.ui.Telescope .. " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+    file_ignore_patterns = {
+      ".git/",
+      "target/",
+      "docs/",
+      "vendor/*",
+      "%.lock",
+      "__pycache__/*",
+      "%.sqlite3",
+      "%.ipynb",
+      "node_modules/*",
+      -- "%.jpg",
+      -- "%.jpeg",
+      -- "%.png",
+      "%.svg",
+      "%.otf",
+      "%.ttf",
+      "%.webp",
+      ".dart_tool/",
+      ".github/",
+      ".gradle/",
+      ".idea/",
+      ".settings/",
+      ".vscode/",
+      "__pycache__/",
+      "build/",
+      "env/",
+      "gradle/",
+      "node_modules/",
+      "%.pdb",
+      "%.dll",
+      "%.class",
+      "%.exe",
+      "%.cache",
+      "%.ico",
+      "%.pdf",
+      "%.dylib",
+      "%.jar",
+      "%.docx",
+      "%.met",
+      "smalljre_*/*",
+      ".vale/",
+      "%.burp",
+      "%.mp4",
+      "%.mkv",
+      "%.rar",
+      "%.zip",
+      "%.7z",
+      "%.tar",
+      "%.bz2",
+      "%.epub",
+      "%.flac",
+      "%.tar.gz",
+    },
+
+    mappings = {
+      i = {
+        ["<C-n>"] = actions.cycle_history_next,
+        ["<C-p>"] = actions.cycle_history_prev,
+
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+
+        ["<C-b>"] = actions.results_scrolling_up,
+        ["<C-f>"] = actions.results_scrolling_down,
+
+        ["<C-c>"] = actions.close,
+
+        ["<Down>"] = actions.move_selection_next,
+        ["<Up>"] = actions.move_selection_previous,
+
+        ["<CR>"] = actions.select_default,
+        ["<C-s>"] = actions.select_horizontal,
+        ["<C-v>"] = actions.select_vertical,
+        ["<C-t>"] = actions.select_tab,
+
+        ["<c-d>"] = require("telescope.actions").delete_buffer,
+
+        -- ["<C-u>"] = actions.preview_scrolling_up,
+        -- ["<C-d>"] = actions.preview_scrolling_down,
+
+        -- ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+        ["<Tab>"] = actions.close,
+        ["<S-Tab>"] = actions.close,
+        -- ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+        ["<C-l>"] = actions.complete_tag,
+        ["<C-h>"] = actions.which_key, -- keys from pressing <C-h>
+        ["<esc>"] = actions.close,
+      },
+
+      n = {
+        ["<esc>"] = actions.close,
+        ["<CR>"] = actions.select_default,
+        ["<C-x>"] = actions.select_horizontal,
+        ["<C-v>"] = actions.select_vertical,
+        ["<C-t>"] = actions.select_tab,
+        ["<C-b>"] = actions.results_scrolling_up,
+        ["<C-f>"] = actions.results_scrolling_down,
+
+        ["<Tab>"] = actions.close,
+        ["<S-Tab>"] = actions.close,
+        -- ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+        -- ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+
+        ["j"] = actions.move_selection_next,
+        ["k"] = actions.move_selection_previous,
+        ["H"] = actions.move_to_top,
+        ["M"] = actions.move_to_middle,
+        ["L"] = actions.move_to_bottom,
+        ["q"] = actions.close,
+        ["dd"] = require("telescope.actions").delete_buffer,
+        ["s"] = actions.select_horizontal,
+        ["v"] = actions.select_vertical,
+        ["t"] = actions.select_tab,
+
+        ["<Down>"] = actions.move_selection_next,
+        ["<Up>"] = actions.move_selection_previous,
+        ["gg"] = actions.move_to_top,
+        ["G"] = actions.move_to_bottom,
+
+        ["<C-u>"] = actions.preview_scrolling_up,
+        ["<C-d>"] = actions.preview_scrolling_down,
+
+        ["<PageUp>"] = actions.results_scrolling_up,
+        ["<PageDown>"] = actions.results_scrolling_down,
+
+        ["?"] = actions.which_key,
+      },
+    },
+  },
+  pickers = {
+
+    live_grep = {
+      theme = "dropdown",
+    },
+    grep_string = {
+      theme = "dropdown",
+    },
+    find_files = {
+      theme = "dropdown",
+      previewer = false,
+    },
+    buffers = {
+      theme = "dropdown",
+      previewer = false,
+      initial_mode = "normal",
+    },
+    planets = {
+      show_pluto = true,
+      show_moon = true,
+    },
+    colorscheme = {
+      -- enable_preview = true,
+    },
+    lsp_references = {
+      theme = "dropdown",
+      initial_mode = "normal",
+    },
+    lsp_definitions = {
+      theme = "dropdown",
+      initial_mode = "normal",
+    },
+    lsp_declarations = {
+      theme = "dropdown",
+      initial_mode = "normal",
+    },
+    lsp_implementations = {
+      theme = "dropdown",
+      initial_mode = "normal",
+    },
+
+
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown {
+        -- even more opts
+      }
+    },
+  },
+}
+
+require("telescope").load_extension("ui-select")
