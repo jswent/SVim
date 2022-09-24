ac5a3ff0f5428243f8b4d31ba1a8234421c966c8
jswent <94636565+jswent@users.noreply.github.com>
2022-09-25 00:31:27 +0100

    fixed global git retrieval

--- lua/utils/git.lua
+++ lua/utils/git.lua
@@ -2,6 +2,11 @@ local M = {}
 
 local if_nil = vim.F.if_nil
 
+local function get_svim_base_dir() 
+  local home = os.getenv("HOME")
+  return home .. "/.config/nvim" 
+end
+
 local function git_cmd(opts)
   local plenary_loaded, Job = pcall(require, "plenary.job")
   if not plenary_loaded then
@@ -9,11 +14,13 @@ local function git_cmd(opts)
   end
 
   opts = opts or {}
+  opts.cwd = opts.cwd or get_svim_base_dir()
 
   local stderr = {}
   local stdout, ret = Job
     :new({
       command = "git",
+      cwd = opts.cwd, 
       args = opts.args,
       on_stderr = function(_, data)
         table.insert(stderr, data)
@@ -31,7 +38,7 @@ function M.get_svim_branch()
 end
 
 function M.get_svim_current_sha() 
-  local args = { "rev-parse", "--short=8", "HEAD" }  
+  local args = { "rev-parse", "--short=7", "HEAD" }  
 
   local _, results = git_cmd { args = args}
   local abbrev_version = if_nil(results[1], "")
@@ -41,7 +48,7 @@ end
 function M.get_svim_version()
   local current_branch = M.get_svim_branch()
 
-  local svim_version = current_branch .. "-" .. M.get_svim_current_sha()
+  local svim_version = current_branch .. "--" .. M.get_svim_current_sha()
   return svim_version
 end
 
