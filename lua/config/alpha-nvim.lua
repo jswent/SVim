9edfe115f6bee380022367b5ed4d8228c93e797e
jswent <94636565+jswent@users.noreply.github.com>
2022-09-24 12:48:11 +0100

    updated dashboard

--- lua/config/alpha-nvim.lua
+++ lua/config/alpha-nvim.lua
@@ -27,20 +27,20 @@ dashboard.section.header.val = {
 
 -- Set menu
 dashboard.section.buttons.val = {
-    dashboard.button("<Leader> f f", icons.documents.Files .. " Find file", ":Telescope find_files <CR>"),
-    dashboard.button("<Leader> f r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
-    dashboard.button("<Leader> f g", icons.ui.List .. " Project Grep", ":Telescope live_grep <CR>"),
-    dashboard.button("u", " Update plugins" , ":PackerSync<CR>"),
-    dashboard.button("e", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
-    dashboard.button("<Leader> n v m", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
-    dashboard.button("q", icons.diagnostics.Error .. " Quit", ":qa<CR>"),
+    dashboard.button("f", icons.documents.Files .. "  > Find file", ":Telescope find_files <CR>"),
+    dashboard.button("r", icons.ui.History .. "  > Recent files", ":Telescope oldfiles <CR>"),
+    dashboard.button("g", icons.ui.List .. "  > Project Grep", ":Telescope live_grep <CR>"),
+    dashboard.button("u", "  > Update plugins" , ":PackerSync<CR>"),
+    dashboard.button("e", icons.ui.NewFile .. "  > New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("c", icons.ui.Gear .. "  > Config", ":e ~/.config/nvim/init.lua <CR>"),
+    dashboard.button("q", icons.diagnostics.Error .. "  > Quit", ":qa<CR>"),
 }
 
 -- local fortune = require("alpha.fortune")
 -- dashboard.section.footer.val = fortune()
 
   local function footer()
-    return "Powered by Swent Technologies -- Release 0.1.0"
+    return "jwswent.com"
   end
 
 dashboard.section.footer.val = footer()
